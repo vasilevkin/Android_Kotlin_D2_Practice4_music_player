59d2c6c0eff8a6facfa13c1faf772335e6c40945
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-04-26 11:19:23 +0300

    Remove old exoplayer video view.

--- app/src/main/java/com/vasilevkin/musicplayer/utils/Constants.kt
+++ app/src/main/java/com/vasilevkin/musicplayer/utils/Constants.kt
@@ -1,5 +1,7 @@
 package com.vasilevkin.musicplayer.utils
 
+import android.net.wifi.WifiManager
+
 
 val Broadcast_PLAY_NEW_AUDIO = "com.vasilevkin.musicplayer.features.playsound.PlayNewAudio"
 val Broadcast_NETWORK_STATE = WifiManager.NETWORK_STATE_CHANGED_ACTION
